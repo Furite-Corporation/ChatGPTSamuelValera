7d2a360c93d606f96db7c8a95704637abd32010b
Luis Valera <luis_valera@mail.ru>
2024-05-09 09:08:25 -0500

    Version 1.1
    Samuel Valera University Task

---
+++ src/flows/welcome.flow.ts
@@ -0,0 +1,8 @@
+import { EVENTS, addKeyword } from "@bot-whatsapp/bot";
+import conversationalLayer from "src/layers/conversational.layer";
+import mainLayer from "src/layers/main.layer";
+
+
+export default addKeyword(EVENTS.WELCOME)
+    .addAction(conversationalLayer)
+    .addAction(mainLayer)
---
+++ src/layers/conversational.layer.ts
@@ -0,0 +1,7 @@
+import { BotContext, BotMethods } from "@bot-whatsapp/bot/dist/types";
+import { handleHistory } from "../utils/handleHistory";
+
+
+export default async ({ body }: BotContext, { state, }: BotMethods) => {
+    await handleHistory({ content: body, role: 'user' }, state)
+}
