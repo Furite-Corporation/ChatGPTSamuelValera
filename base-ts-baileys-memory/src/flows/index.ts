d41a0150dedc360d312472ffc105b32838c01ed3
Luis Valera <luis_valera@mail.ru>
2024-05-14 22:11:07 -0500

    Version20
    Version 2.0 ChatBoot

---
+++ base-ts-baileys-memory/src/flows/index.ts
@@ -0,0 +1,7 @@
+import { createFlow } from "@bot-whatsapp/bot";
+import welcomeFlow from "./welcome.flow";
+import { flowSeller } from "./seller.flow";
+import { flowSchedule } from "./schedule.flow";
+import { flowConfirm } from "./confirm.flow";
+
+export default createFlow([welcomeFlow, flowSeller, flowSchedule, flowConfirm])
